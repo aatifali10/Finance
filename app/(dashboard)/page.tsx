8bbee8ef228fb9e85028e23119f5d8c12d340f14
Aatif Ali <aatifwarraich10@gmail.com>
2024-05-11 22:27:07 +0500

    clerk authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <Button variant="outline">Button</Button>
-    
-
-  );
+  return <p>This is authentication routes</p>;
 }
